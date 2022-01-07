32f2982c8d781501f2f5e5ed1101311f0ef7a70e
Milad Khoshdel <miladkhoshdel@gmail.com>
2022-01-07 23:10:20 +0330

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost')
 channel = connection.channel()
 channel.queue_declare(queue='regis_ord')
 
-
 def on_res(ch, method, properties, body):
     body = body.decode()
     print(f'Checking {body}')
